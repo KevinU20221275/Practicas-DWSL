8b6325d28bfb26d0276f421988f06df5ce636b47
KevinU20221275 <U20221275@univo.edu.sv>
2024-09-11 12:02:10 -0600

    practica 5

---
+++ Practica-5/config/database.php
@@ -0,0 +1,30 @@
+<?php
+class Database{
+    private $host = "localhost";
+
+    private $database = "dbtienda";
+
+    private $user = "root";
+
+    private $password = "";
+
+    private $conn;
+
+    public function getConnection(){
+        $this->conn = null;
+        try {
+            $this->conn = new PDO(
+                "mysql:host".$this->$host.";dbname=".this->database,
+                $this->user, $this->password
+            );
+
+            $this->conn->exec("set names utf8");
+        } catch (PDOExeption $e) {
+            echo "Connection error ". $e->getMessage();
+            die();
+        }
+        return $this->conn;
+    }
+}
+
+?>
---
+++ Practica-5/models/Productos.php
@@ -0,0 +1,97 @@
+<?php
+class Productos{
+    private $conn;
+
+    private $table_name = "productos";
+
+    public $id;
+
+    public $name;
+
+    public $description;
+
+    public $categorie;
+
+    public function __construct($db){
+        $this->conn = $db;
+    }
+
+    public function create(){
+        $query = "INSERT INTO ".$this->table_name." (name, description, categorie) VALUES (:name, :description, :categorie)";
+        $resultt = $this->conn->prepare($query);
+
+        $this->name = htmlspecialchars(strip_tags($this-name));
+        $this->description = htmlspecialchars(strip_tags($this-description));
+        $this->categorie = htmlspecialchars(strip_tags($this-categorie));
+        
+        $result->bindParam(":name", $this->name);
+        $result->bindParam(":description", $this->description);
+        $result->bindParam(":categorie", $this->categorie);
+
+        if ($result->execute()){
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public function get_products(){
+        $query = "SELECT * FROM ".$this->table_name;
+        $resultt = $this->conn->prepare($query);
+        $result->execute();
+        return $result;
+    }
+
+    public function get_product_by_id(){
+        $query = "SELECT * FROM ".$this->table_name.
+        " WHERE id=:id";
+        $resultt = $this->conn->prepare($query);
+        result->bindParam(":id", $this->id);
+        $result->execute();
+
+        $row = $result->fetch(PDO::FETCH_ASSOC);
+
+        $this->name = $row["name"];
+        $this->description = $row["description"];
+        $this->categorie = $row["categorie"];
+
+        return $result;
+    }
+
+    public function update(){
+        $query = "UPDATE ".$this->table_name." SET name=:name, description=:description, categorie=:categorie 
+        WHERE id=:id";
+        $resultt = $this->conn->prepare($query);
+
+        $this->name = htmlspecialchars(strip_tags($this-name));
+        $this->description = htmlspecialchars(strip_tags($this-description));
+        $this->categorie = htmlspecialchars(strip_tags($this-categorie));
+        $this->id = htmlspecialchars(strip_tags($this-id));
+        
+        $result->bindParam(":name", $this->name);
+        $result->bindParam(":description", $this->description);
+        $result->bindParam(":categorie", $this->categorie);
+        $result->bindParam(":id", $this->id);
+
+        if ($result->execute()){
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public function delete(){
+        $query = "DELETE FROM ".$this->table_name.
+        " WHERE id=:id";
+        $resultt = $this->conn->prepare($query);
+        result->bindParam(":id", $this->id);
+
+        if ($result->execute()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
+
+?>
